8a41a7eb8906d5e2119584978c0bc93f452103ae
young <rhsiddlsidhd@nate.com>
2025-09-19 20:50:10 +0900

    remove utils trnsform file

--- src/utils/transform.ts
+++
@@ -1,8 +0,0 @@
-import { Book } from "../constants/booksConstants";
-import { Mode } from "../stores/useScreenStore";
-
-export const getVisibleBooks = (books: Book[], mode: Mode, intro: string) => {
-  return mode !== "mobile"
-    ? books.filter((book) => book.description !== intro)
-    : books;
-};
