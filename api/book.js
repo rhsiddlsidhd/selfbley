b0a906f15d62727fc24304daf8fcaf33342c6ca5
young <rhsiddlsidhd@nate.com>
2025-09-23 20:12:37 +0900

    serverless fn bug

--- api/book.js
+++ api/book.js
@@ -3,12 +3,16 @@ import fs from "fs";
 
 function handler(req, res) {
   const filePath = path.join(process.cwd(), "db.json");
+  console.log("filePath", filePath);
   const jsonData = fs.readFileSync(filePath, "utf-8");
+  console.log("jsonData", jsonData);
   const booksData = JSON.parse(jsonData);
+  console.log("booksData", booksData);
 
   const { q } = req.query;
 
   const book = booksData.books.find((b) => b.id === q);
+  console.log("book", book);
 
   if (!book)
     return res
