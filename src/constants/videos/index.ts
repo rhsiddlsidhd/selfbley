1b499e2cc44939e1dec820474aac84d13d8b8eac
young <rhsiddlsidhd@nate.com>
2025-09-29 16:30:43 +0900

    edit: deployment Error resolve

--- src/constants/videos/index.ts
+++ src/constants/videos/index.ts
@@ -2,10 +2,6 @@ import snowboard_webm from "../../assets/video/webm/snowboardS.webm";
 import programming_webm from "../../assets/video/webm/programming.webm";
 import run_webm from "../../assets/video/webm/runS.webm";
 import tennis_webm from "../../assets/video/webm/tennis.webm";
-import snowboard_mp4 from "../../assets/video/mp4/snowboard.mp4";
-import run_mp4 from "../../assets/video/mp4/run.mp4";
-import tennis_mp4 from "../../assets/video/mp4/tennis.mp4";
-import programming_mp4 from "../../assets/video/mp4/programming.mp4";
 
 export const homeVideos = [
   { webm: tennis_webm },
