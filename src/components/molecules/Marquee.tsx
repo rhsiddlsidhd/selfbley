b9ddb60d22d394aa7f974bbba8f8c427a8b8c411
Young <rhsiddlsidhd@nate.com>
2025-10-02 12:30:47 +0900

    Marquee test

--- src/components/molecules/Marquee.tsx
+++ src/components/molecules/Marquee.tsx
@@ -1,7 +1,7 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useRef } from "react";
 import styled from "styled-components";
 import { motion } from "motion/react";
-import Heading from "../atoms/Heading";
+
 interface MarqueeTextProps {
   text: string;
   reverse?: boolean;
@@ -9,54 +9,10 @@ interface MarqueeTextProps {
 }
 
 const Marquee = ({ deg = 0, reverse = false, text }: MarqueeTextProps) => {
-  const [isMounted, setIsMounted] = useState(false);
-  const [multiplier, setMultiplier] = useState(1);
   const containerRef = useRef<HTMLDivElement>(null);
-  const textRef = useRef<HTMLHeadingElement>(null);
-
-  const calculateMultiplier = useCallback(() => {
-    if (!containerRef.current || !textRef.current) return;
-
-    const containerRect = containerRef.current.getBoundingClientRect();
-    const textRect = textRef.current.getBoundingClientRect();
-
-    const containerWidth = containerRect.width;
-    const textWidth = textRect.width;
-
-    if (textWidth < containerWidth) {
-      setMultiplier(Math.ceil(containerWidth / textWidth));
-    }
-  }, []);
-
-  const multiplyChildren = useCallback(
-    (multiplier: number) => {
-      const arraySize = Math.max(multiplier, 0);
-
-      return [...Array(arraySize)].map((_, i) => (
-        <Heading key={i} renderContent={text} />
-      ));
-    },
-    [text]
-  );
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  useEffect(() => {
-    if (!isMounted) return;
-
-    calculateMultiplier();
-    if (containerRef.current) {
-      const resizeObserver = new ResizeObserver(() => calculateMultiplier());
-      resizeObserver.observe(containerRef.current);
-
-      return () => resizeObserver.disconnect();
-    }
-  }, [calculateMultiplier, isMounted]);
 
   const marqueeAnimation = {
-    x: reverse ? [0, "100%"] : [0, "-100%"],
+    x: reverse ? [0, "100%"] : [0, "-50%"],
     transition: {
       duration: 10,
       ease: "linear",
@@ -64,38 +20,60 @@ const Marquee = ({ deg = 0, reverse = false, text }: MarqueeTextProps) => {
     },
   };
 
-  if (!isMounted) return null;
-
   return (
-    <MarqueeTrack $deg={deg} $reverse={reverse} ref={containerRef}>
-      {Array.from({ length: 2 }, (_, i) => {
-        return (
-          <MarqueeMessage animate={marqueeAnimation} key={i}>
-            <Heading ref={textRef} renderContent={text} />
-            {multiplyChildren(multiplier - 1)}
-          </MarqueeMessage>
-        );
-      })}
+    <MarqueeTrack ref={containerRef}>
+      <MarqueeMessage
+        animate={marqueeAnimation}
+        transition={{ ease: "linear" }}
+        $deg={deg}
+        $reverse={reverse}
+      >
+        {Array.from({ length: 2 }, (_, i) => {
+          return (
+            <Text key={i}>
+              {[...text].map((word, i) => {
+                return (
+                  <motion.span
+                    initial={{ filter: "blur(0px)" }}
+                    animate={{
+                      filter: "blur(10px)",
+                      transition: {
+                        delay: i * 0.25,
+                        duration: 3,
+                        repeat: Infinity,
+                      },
+                    }}
+                    key={`${i}-${word}`}
+                  >
+                    {word}
+                  </motion.span>
+                );
+              })}
+            </Text>
+          );
+        })}
+      </MarqueeMessage>
     </MarqueeTrack>
   );
 };
 
 export default Marquee;
 
-const MarqueeTrack = styled.div<{ $deg: number; $reverse: boolean }>`
-  display: flex;
+const MarqueeTrack = styled.div`
   white-space: nowrap;
-  flex-shrink: 0;
-  font-weight: bold;
-  justify-content: ${({ $reverse }) => ($reverse ? "end" : "start")};
-  transform: ${({ $deg }) => `rotate(${$deg}deg)`};
-  padding: 3rem 0;
+  font-size: clamp(4rem, 25vw, 14rem);
 `;
 
-const MarqueeMessage = styled(motion.div)`
+const MarqueeMessage = styled(motion.div)<{ $deg: number; $reverse: boolean }>`
   display: flex;
+  justify-content: ${({ $reverse }) => ($reverse ? "end" : "start")};
+  transform: ${({ $deg }) => `rotate(${$deg}deg)`};
   cursor: pointer;
-  & > * {
-    margin: 0 3rem;
-  }
+  will-change: transform;
+`;
+
+const Text = styled.h1`
+  font-size: inherit;
+  padding: 0 1rem;
+  z-index: 90;
 `;
